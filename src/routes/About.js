f307c9412032a1b0adca90a4d34b85646ec7135f
younghwani <younghwankim624@gmail.com>
2021-02-28 14:42:34 +0900

    route

---
+++ src/routes/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+	return (
+		<div className="about__container">
+			<span>About this page: I built it because I love Movies!!</span>
+			<span>Younghwani</span>
+		</div>
+	);
+}
+
+export default About;
