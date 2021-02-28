d38aa0d468ecf90fe9cf0d294e9933652ec8061e
younghwani <younghwankim624@gmail.com>
2021-02-28 15:16:55 +0900

    detail style

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Detail.css";
 
 class Detail extends React.Component {
 	componentDidMount() {
@@ -10,7 +11,27 @@ class Detail extends React.Component {
 	render() {
 		const { location } = this.props;
 		if (location.state) {
-			return <span>{location.state.title}</span>;
+			return (
+				<div className="detail__container">
+					<img
+						className="detail__poster"
+						src={location.state.poster}
+						alt={location.state.title}
+						title={location.state.title}
+					></img>
+					<h1 className="detail__title">{location.state.title}</h1>
+					<span>made .. </span>
+					<span className="detail__year">{location.state.year}</span>
+					<p>genre</p>
+					<ul className="detail__genres">
+						{location.state.genres.map((genre, index) => (
+							<li key={index}>{genre}</li>
+						))}
+					</ul>
+					<p>Summary</p>
+					<p className="detail__summary">{location.state.summary}</p>
+				</div>
+			);
 		} else {
 			return null;
 		}
